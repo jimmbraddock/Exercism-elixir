455ccdd679982f6483952c9052c30b8d55086750
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-01-26 09:17:13 +0300

    gigasecond

--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -1,10 +1,14 @@
 defmodule Gigasecond do
+
+    @gigasecond 1000000000
+
 	@doc """
 	Calculate a date one billion seconds after an input date.
 	"""
 	@spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
 
-	def from({{year, month, day}, {hours, minutes, seconds}}) do
-
+	def from(date) do
+        @gigasecond + :calendar.datetime_to_gregorian_seconds(date)
+        |> :calendar.gregorian_seconds_to_datetime()
 	end
 end
--- gigasecond/gigasecond_test.exs
+++ gigasecond/gigasecond_test.exs
@@ -15,12 +15,12 @@ defmodule GigasecondTest do
     assert Gigasecond.from({{2011, 4, 25}, {0, 0, 0}}) == {{2043, 1, 1}, {1, 46, 40}}
   end
 
-  @tag :pending
+  #@tag :pending
   test "from 6/13/1977" do
     assert Gigasecond.from({{1977, 6, 13}, {0, 0, 0}}) == {{2009, 2, 19}, {1, 46, 40}}
   end
 
-  @tag :pending
+  #@tag :pending
   test "from 7/19/1959" do
     assert Gigasecond.from({{1959, 7, 19}, {0, 0, 0}}) == {{1991, 3, 27}, {1, 46, 40}}
   end
@@ -28,8 +28,8 @@ defmodule GigasecondTest do
   @tag :pending
   test "yourself" do
     # customize these values for yourself
-    # your_birthday = {{year1, month1, day1}, {0, 0, 0}}
-    # assert Gigasecond.from(your_birthday) == {{year2, month2, day2}, {hours, minutes, seconds}}
+    #your_birthday = {{1991, 7, 1}, {0, 0, 0}}
+    #assert Gigasecond.from(your_birthday) == {{year2, month2, day2}, {hours, minutes, seconds}}
   end
 end
 
