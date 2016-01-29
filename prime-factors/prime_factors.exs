5b1a59b1830029387398d24a16b8c60d32b8c38a
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-01-29 11:31:34 +0300

    'primefactor'

--- prime-factors/prime_factors.exs
+++ prime-factors/prime_factors.exs
@@ -9,6 +9,17 @@ defmodule PrimeFactors do
   """
   @spec factors_for(pos_integer) :: [pos_integer]
   def factors_for(number) do
-
+    number
+    |> factorize(2, [])
   end
+
+  defp factorize(number, factor, prime_factors) when number < factor, do: prime_factors
+  defp factorize(number, factor, prime_factors) when rem(number, factor) == 0 do
+    [factor|factorize(div(number, factor), factor, prime_factors)]
+  end
+  defp factorize(number, factor, prime_factors) do
+    number
+    |> factorize(factor + 1, prime_factors)
+  end
+
 end
