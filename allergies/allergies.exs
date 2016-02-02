2e352165f649b25d9d2471334c49b47679dc4b01
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-02-02 10:05:54 +0300

    allergies ver2

--- allergies/allergies.exs
+++ allergies/allergies.exs
@@ -1,30 +1,28 @@
 defmodule Allergies do
 
-@material [ 
-  "eggs",
-  "peanuts", 
-  "shellfish", 
-  "strawberries",
-  "tomatoes",
-  "chocolate",
-  "pollen",
-  "cats"
-]
+use Bitwise
+
+@material %{
+  "eggs" => 1,
+  "peanuts" => 2, 
+  "shellfish" => 4, 
+  "strawberries" => 8,
+  "tomatoes" => 16,
+  "chocolate" => 32,
+  "pollen" => 64,
+  "cats" => 128
+}
 
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t]
   def list(flags) do
-    flags
-    |> Integer.digits(2)
-    |> Enum.reverse
-    |> Enum.zip(@material)
-    |> Enum.reduce([], fn({flag, mat}, acc) when flag == 1 ->
-                         List.insert_at(acc, -1, mat)
-                       (_, acc) ->
-                         acc
-    end)
+    Enum.reduce(@material, [], fn {k, v}, acc
+        when (flags &&& v) != 0 -> [k|acc]
+        (_, acc) -> acc
+      end
+    )
   end
 
 
@@ -33,6 +31,6 @@ defmodule Allergies do
   """
   @spec allergic_to?(non_neg_integer, String.t) :: boolean
   def allergic_to?(flags, item) do
-    item in list(flags)
+    (flags &&& @material[item]) != 0
   end
 end
