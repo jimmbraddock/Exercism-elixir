f47a81234951fcedcd876c6ddc273136100d2d23
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-02-02 17:05:26 +0300

    atbash-cipher

--- atbash-cipher/atbash.exs
+++ atbash-cipher/atbash.exs
@@ -9,6 +9,24 @@ defmodule Atbash do
   """
   @spec encode(String.t) :: String.t
   def encode(plaintext) do
-  
+    plaintext
+    |> String.downcase
+    |> String.replace(~r/[^\w\d]/, "")
+    |> String.graphemes
+    |> encode([])
+    |> Enum.chunk(5, 5, [])
+    |> Enum.map(&Enum.join(&1, ""))
+    |> Enum.join(" ") 
   end
+
+  defp encode([], acc), do: acc
+  defp encode([h|t], acc) do
+      encode(t, acc ++ [convert(h)])
+  end
+
+  def convert(_=<<nb>>) when nb >= 97 and nb <= 122, do: <<219 - nb>>
+  def convert(n), do: n
+
 end
+
+
--- atbash-cipher/atbash_cipher_test.exs
+++ atbash-cipher/atbash_cipher_test.exs
@@ -5,7 +5,7 @@ else
 end
 
 ExUnit.start
-ExUnit.configure exclude: :pending, trace: true
+#ExUnit.configure exclude: :pending, trace: true
 
 defmodule AtbashTest do
   use ExUnit.Case, async: true
@@ -15,37 +15,37 @@ defmodule AtbashTest do
     assert Atbash.encode("no") == "ml"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode yes" do
     assert Atbash.encode("yes") == "bvh"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode OMG" do
     assert Atbash.encode("OMG") == "lnt"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode O M G" do
     assert Atbash.encode("O M G") == "lnt"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode long word" do
     assert Atbash.encode("mindblowingly") == "nrmwy oldrm tob"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode numbers" do
     assert Atbash.encode("Testing, 1 2 3, testing.") == "gvhgr mt123 gvhgr mt"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode sentence" do
     assert Atbash.encode("Truth is fiction.") == "gifgs rhurx grlm"
   end
 
-  @tag :pending
+  #@tag :pending
   test "encode all the things" do
     plaintext = "The quick brown fox jumps over the lazy dog."
     cipher = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
