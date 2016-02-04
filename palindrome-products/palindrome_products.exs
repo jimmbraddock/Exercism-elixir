9f9b4df500f08290c1182700d89eb1cdf0cf8fe2
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-02-04 16:36:49 +0300

    palindrome

--- palindrome-products/palindrome_products.exs
+++ palindrome-products/palindrome_products.exs
@@ -5,6 +5,18 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map() 
   def generate(max_factor, min_factor \\ 1) do
-    
+    max_factor
+    |> _generate_palindroms(min_factor)
+    |> Enum.reduce( %{}, fn [h|t], acc ->
+          Map.update(acc, String.to_integer(h), t, &(&1 ++ t) )
+       end)
+  end
+
+  def _generate_palindroms(max, min) do
+    for a <- min..max,
+        b <- a..max,
+        "#{a*b}" == String.reverse("#{a*b}"),
+        into: [],
+        do: ["#{a*b}", [a, b]]
   end
 end
