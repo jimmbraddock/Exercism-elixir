7e05c1a34e0594e0cf2bf85395311a93f64f8a80
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-02-04 17:02:00 +0300

    palindrome

--- palindrome-products/palindrome_products.exs
+++ palindrome-products/palindrome_products.exs
@@ -7,16 +7,14 @@ defmodule Palindromes do
   def generate(max_factor, min_factor \\ 1) do
     max_factor
     |> _generate_palindroms(min_factor)
-    |> Enum.reduce( %{}, fn [h|t], acc ->
-          Map.update(acc, String.to_integer(h), t, &(&1 ++ t) )
-       end)
+    |> Enum.reverse
+    |> Enum.group_by(fn [a, b] -> a*b end)
   end
 
   def _generate_palindroms(max, min) do
     for a <- min..max,
         b <- a..max,
         "#{a*b}" == String.reverse("#{a*b}"),
-        into: [],
-        do: ["#{a*b}", [a, b]]
+        do: [a,b]
   end
 end
