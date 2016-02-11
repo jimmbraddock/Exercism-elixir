58f6a80b1368ef2bbb135670a7ec4977f72193c0
e.vasilyev <e.vasilyev@argustelecom.ru>
2016-02-11 11:05:18 +0300

    custom-set

--- custom-set/custom_set.exs
+++ custom-set/custom_set.exs
@@ -1,5 +1,35 @@
 defmodule CustomSet do
-  # This lets the compiler check that all Set callback functions have been
+  # This lets the compiler check that all list callback functions have been
   # implemented.
   @behaviour Set
+
+  defstruct list: []
+
+  def delete(t, value), do: new(Enum.filter(t.list, &(&1 !== value)))
+
+  def difference(t1, t2), do: new(Enum.reject(t1.list, &(member?(t2, &1))))
+
+  def disjoint?(t1, t2), do: size(intersection(t1,t2)) == 0
+
+  def equal?(t1, t2), do: Enum.sort(t1.list) === Enum.sort(t2.list)
+
+  def intersection(t1, t2), do: new(Enum.filter(t1.list, &(member?(t2, &1))))
+
+  def member?(t, value), do: value in t.list
+
+  def put(t=%CustomSet{list: values}, value), do: new([value|values])
+  
+  def empty(_), do: new()
+  
+  def new(),   do: %CustomSet{}
+  def new([]), do: %CustomSet{}
+  def new(l), do: %CustomSet{list: l |> Enum.uniq |> Enum.sort}
+
+  def size(t), do: length(t.list)
+
+  def subset?(t1, t2), do: Enum.all?(t1.list, &(member?(t2, &1)))
+
+  def to_list(t), do: Enum.sort(t.list)
+
+  def union(t1, t2), do: new(t1.list ++ t2.list)
 end
--- custom-set/custom_set_test.exs
+++ custom-set/custom_set_test.exs
@@ -5,7 +5,7 @@ else
 end
 
 ExUnit.start
-ExUnit.configure exclude: :pending, trace: true
+#ExUnit.configure exclude: :pending, trace: true
 
 defmodule CustomSetTest do
   use ExUnit.Case
@@ -14,14 +14,14 @@ defmodule CustomSetTest do
   # is a tuple with as first element `CustomSet`. A record with tag `CustomSet`
   # will meet this requirement.
 
-  # @tag :pending
+  #@tag :pending
   test "delete" do
     assert Set.equal?(Set.delete(CustomSet.new([3,2,1]), 2), CustomSet.new([1,3]))
     assert Set.equal?(Set.delete(CustomSet.new([3,2,1]), 4), CustomSet.new([1,2,3]))
     assert Set.equal?(Set.delete(CustomSet.new([3,2,1]), 2.0), CustomSet.new([1,2,3]))
   end
 
-  @tag :pending
+  #@tag :pending
   test "difference" do
     assert Set.equal?(
       Set.difference(CustomSet.new([1,2,3]), CustomSet.new([2,4])),
@@ -31,7 +31,7 @@ defmodule CustomSetTest do
       CustomSet.new([1,2.0,3]))
   end
 
-  @tag :pending
+  #@tag :pending
   test "disjoint?" do
     assert Set.disjoint?(CustomSet.new([1,2]), CustomSet.new([3,4]))
     refute Set.disjoint?(CustomSet.new([1,2]), CustomSet.new([2,3]))
@@ -39,13 +39,13 @@ defmodule CustomSetTest do
     assert Set.disjoint?(CustomSet.new, CustomSet.new)
   end
 
-  @tag :pending
+  #@tag :pending
   test "empty" do
     assert Set.equal?(Set.empty(CustomSet.new([1,2])), CustomSet.new)
     assert Set.equal?(Set.empty(CustomSet.new), CustomSet.new)
   end
 
-  @tag :pending
+  #@tag :pending
   test "intersection" do
     assert Set.equal?(
       Set.intersection(CustomSet.new([:a, :b, :c]), CustomSet.new([:a, :c, :d])),
@@ -55,7 +55,7 @@ defmodule CustomSetTest do
       CustomSet.new([3]))
   end
 
-  @tag :pending
+  #@tag :pending
   test "member?" do
     assert Set.member?(CustomSet.new([1,2,3]), 2)
     assert Set.member?(CustomSet.new(1..3), 2)
@@ -63,7 +63,7 @@ defmodule CustomSetTest do
     refute Set.member?(CustomSet.new(1..3), 4)
   end
 
-  @tag :pending
+  #@tag :pending
   test "put" do
     assert Set.equal?(
       Set.put(CustomSet.new([1,2,4]), 3),
@@ -76,14 +76,14 @@ defmodule CustomSetTest do
       CustomSet.new([1,2,3,3.0]))
   end
 
-  @tag :pending
+  #@tag :pending
   test "size" do
     assert Set.size(CustomSet.new) == 0
     assert Set.size(CustomSet.new([1,2,3])) == 3
     assert Set.size(CustomSet.new([1,2,3,2])) == 3
   end
 
-  @tag :pending
+  #@tag :pending
   test "subset?" do
     assert Set.subset?(CustomSet.new([1,2,3]), CustomSet.new([1,2,3]))
     assert Set.subset?(CustomSet.new([1,2,3]), CustomSet.new([4,1,2,3]))
@@ -93,14 +93,14 @@ defmodule CustomSetTest do
     assert Set.subset?(CustomSet.new, CustomSet.new)
   end
 
-  @tag :pending
+  #@tag :pending
   test "to_list" do
     assert Enum.sort(Set.to_list(CustomSet.new)) == []
     assert Enum.sort(Set.to_list(CustomSet.new([3,1,2]))) == [1,2,3]
     assert Enum.sort(Set.to_list(CustomSet.new([3,1,2,1]))) == [1,2,3]
   end
 
-  @tag :pending
+  #@tag :pending
   test "union" do
     assert Set.equal?(
       CustomSet.union(CustomSet.new([1,3]), CustomSet.new([2])),
@@ -119,7 +119,7 @@ defmodule CustomSetTest do
       CustomSet.new([]))
   end
 
-  @tag :pending
+  #@tag :pending
   test "inspect" do
     assert inspect(CustomSet.new) == "%CustomSet{list: []}"
     assert inspect(CustomSet.new([1,3,2])) == "%CustomSet{list: [1, 2, 3]}"
