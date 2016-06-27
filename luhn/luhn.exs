7a3647b20e90483b6cfc865efe334ecf15192c59
Eugenie.Vasilyev <eugenie.vasilyev@infon.ru>
2016-06-27 11:20:33 +0300

    Add luhn exercise

--- luhn/luhn.exs
+++ luhn/luhn.exs
@@ -4,6 +4,26 @@ defmodule Luhn do
   """
   @spec checksum(String.t()) :: integer
   def checksum(number) do
+    number
+      |> String.to_integer
+      |> Integer.digits
+      |> Enum.reverse
+      |> Enum.with_index(1)
+      |> Enum.map(fn {el, i} ->
+        if rem(i, 2) == 0 do
+          {el * 2, i}
+        else
+          {el, i}
+        end
+      end)
+      |> Enum.map(fn {el, _} ->
+        if el > 9 do
+          el - 9
+        else
+            el
+        end
+      end)
+      |> Enum.sum
   end
 
   @doc """
@@ -11,6 +31,9 @@ defmodule Luhn do
   """
   @spec valid?(String.t()) :: boolean
   def valid?(number) do
+    number
+    |> checksum
+    |> rem(10) == 0
   end
 
   @doc """
@@ -19,5 +42,16 @@ defmodule Luhn do
   """
   @spec create(String.t()) :: String.t()
   def create(number) do
+    number <> get_last_number(number)
   end
+
+  defp get_last_number(number) do
+    number <> "0"
+    |> checksum
+    |> rem(10)
+    |> (&(10 - &1)).()
+    |> Integer.to_string
+    |> String.last
+  end
+
 end
